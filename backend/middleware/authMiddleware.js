2e1523370e556f78d0d93bc8d80c2580c36dbaeb
YonugJones <peterckerfoot@gmail.com>
2025-02-09 17:16:20 -0600

    created protect middleware function

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,3 +3,24 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const CustomError = require('../errors/customError')
 
+const protect = asyncHandler(async (req, res, next) => {
+  let token = req.headers.authorization
+
+  if (!token || !token.startsWith('Bearer ')) {
+    throw new CustomError('Unauthorized: no token provided', 401)
+  }
+
+  try {
+    token = token.split(' ')[1]
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    req.user = { id: decoded.id, username: decoded.username }
+
+    next()
+  } catch (err) {
+    throw new CustomError('Unauthorized: invalid token', 401)
+  }
+}) 
+
+module.exports = {
+  protect
+}
--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,3 +1,5 @@
+// global errorHandler
+
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500
   const message = err.message || 'Internal server error'
