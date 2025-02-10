6c313770c0d2e680a20dcfd0edabe1314c21cc73
YonugJones <peterckerfoot@gmail.com>
2025-02-10 16:53:21 -0600

    created getAllPosts function

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -64,7 +64,80 @@ const editPost = asyncHandler(async (req, res) => {
   })
 })
 
+const deletePost = asyncHandler(async (req,res) => {
+  const user = req.user
+  if (!user) {
+    throw new CustomError('Unauthorized: user not authenticated', 401)
+  }
+
+  const postId = parseInt(req.params.postId, 10);
+  if (isNaN(postId)) {
+    throw new CustomError('Invalid Post ID', 400)
+  }
+
+  const post = await prisma.post.findUnique({ where: { id: postId } })
+  if (!post) {
+    throw new CustomError('Post not found', 404)
+  }
+
+  if (post.userId !== user.id) {
+    throw new CustomError('Unauthorized: only post author can delete post', 403)
+  }
+
+  await prisma.post.delete({ where: { id: postId } })
+
+  res.status(200).json({
+    success: true,
+    message: 'Post deleted'
+  })
+})
+
+const getAllPosts = asyncHandler(async (req, res) => {
+  const posts = await prisma.post.findMany({
+    orderBy: { createdAt: 'desc' },
+    include: {
+      user: {
+        select: { 
+          id:true, 
+          username: true, 
+          profilePic: true 
+        }
+      },
+      comments: {
+        orderBy: { createdAt: 'desc' },
+        select: {
+          id: true,
+          postId: true,
+          content: true,
+          createdAt: true,
+          user: {
+            select: { 
+              id: true, 
+              username: true, 
+              profilePic: true 
+            }
+          },
+          _count: {
+            select: { likes: true }
+          }
+        }
+      },
+      _count: {
+        select: { likes: true }
+      }
+    }
+  })
+
+  res.status(200).json({
+    success: true,
+    message: 'All posts fetched',
+    data: posts
+  })
+})
+
 module.exports = {
   newPost,
-  editPost
+  editPost,
+  deletePost,
+  getAllPosts
 }
--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const { newPost, editPost } = require('../controllers/postController')
+const { newPost, editPost, deletePost, getAllPosts } = require('../controllers/postController')
 const { authenticateToken } = require('../middleware/authMiddleware')
 const router = express.Router()
 
 router.post('/', authenticateToken, newPost)
 router.put('/:postId', authenticateToken, editPost)
+router.delete('/:postId', authenticateToken, deletePost)
+router.get('/', authenticateToken, getAllPosts)
 
 module.exports = router
