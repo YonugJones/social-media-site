aa89ca498227f55c1370b5a33ae377606b788eaf
YonugJones <peterckerfoot@gmail.com>
2025-02-11 17:37:34 -0600

    created followRequest function added followRequest to userRoute

--- backend/controllers/friendshipController.js
+++ backend/controllers/friendshipController.js
@@ -1,4 +1,41 @@
-// const prisma = require('../prisma/prismaClient')
-// const asyncHandler = require('express-async-handler')
-// const CustomError = require('../errors/customError')
+const prisma = require('../prisma/prismaClient')
+const asyncHandler = require('express-async-handler')
+const CustomError = require('../errors/customError')
 
+const followRequest = asyncHandler(async (req, res) => {
+  const user = req.user
+  if (!user) {
+    throw new CustomError('Unauthorized: user not authenticated', 401)
+  }
+  
+  const userId = user.id
+
+  const followId = parseInt(req.params.userId, 10)
+  if (isNaN(followId)) {
+    throw new CustomError('Invalid user ID', 400)
+  }
+
+  if (userId === followId) {
+    throw new CustomError('You cannot follow yourself', 400)
+  }
+
+  const existingFriendship = await prisma.friendship.findFirst({
+    where: { followerId: userId, followingId: followId }
+  })
+  if (existingFriendship) {
+    return res.status(409).json({
+      success: false,
+      message: 'Follow request already sent'
+    })
+  }
+
+  await prisma.friendship.create({
+    data: { followerId: userId, followingId: followId }
+  })
+
+  res.status(201).json({ success: true, message: 'Follow request sent' })
+})
+
+module.exports = {
+  followRequest
+}
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const postController = require('../controllers/postController')
 const userController = require('../controllers/userController')
+const friendshipController = require('../controllers/friendshipController')
 const { authenticateToken } = require('../middleware/authMiddleware')
 const router = express.Router()
 
 // /users
-router.get('/:userId', authenticateToken, userController.getUser)
 router.get('/:userId/posts', authenticateToken, postController.getPostsByUser)
+
+router.get('/:userId', authenticateToken, userController.getUser)
 router.put('/:userId', authenticateToken, userController.editUser)
 router.delete('/:userId', authenticateToken, userController.deleteUser)
+
+router.post('/:userId/follow', authenticateToken, friendshipController.followRequest)
 
 module.exports = router;
