f4c0731bca6b893adddc3eefc31d8115421ff13a
YonugJones <peterckerfoot@gmail.com>
2025-02-25 16:58:45 -0600

    created PostDetails added propTypes validation

--- frontend/src/api/postApi.js
+++ frontend/src/api/postApi.js
@@ -1,1 +1,11 @@
-// import { axiosPrivate } from '../utils/axios'
+import { axiosPrivate } from '../utils/axios'
+
+export const getFeedPosts = async () => {
+  try {
+    const response = await axiosPrivate.post('/posts/feed')
+    return response.data
+  } catch (err) {
+    console.error('API error:', err)
+    throw err
+  }
+}
