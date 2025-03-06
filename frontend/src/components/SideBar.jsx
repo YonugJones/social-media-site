efbf8f8a3fc738f0a3c5c00039f0fd6ac9a9ab01
YonugJones <peterckerfoot@gmail.com>
2025-03-06 11:11:31 -0600

    added Home link in Sidebar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import styles from '../styles/SideBar.module.css'
 
 const SideBar = () => {
   return (
     <div className={styles['side-bar']}>
       <ul>
+        <li><Link to='/' className={styles['home-link']}>HOME</Link></li>
         <li>NEW POST</li>
         <li>NEW MESSAGE</li>
         <li>FOLLWERS</li>
