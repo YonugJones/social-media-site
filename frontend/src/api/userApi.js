2a1db20f8ab841cb4a5f98d1d3e694bf72b7e23e
YonugJones <peterckerfoot@gmail.com>
2025-03-11 12:19:48 -0500

    created getPostsByUser api function

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -7,3 +7,12 @@ export const getUser = async (axiosPrivateInstance, userId) => {
     throw err
   }
 }
+
+export const getPostByUser = async (axiosPrivateInstance, userId) => {
+  try {
+    const response = await axiosPrivateInstance.get(`/users/${userId}/posts`)
+    return response.data
+  } catch (err) {
+    console.error('API error:', err)
+  }
+}
