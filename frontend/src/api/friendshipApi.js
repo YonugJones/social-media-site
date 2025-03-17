41110aa155085184ea5d5845fdfd45e13eb6b0a8
YonugJones <peterckerfoot@gmail.com>
2025-03-17 14:07:45 -0500

    created confrimFollowRequest api function

--- frontend/src/api/friendshipApi.js
+++ frontend/src/api/friendshipApi.js
@@ -8,3 +8,12 @@ export const followRequest = async (axiosPrivateInstance, userId) => {
   }
 }
 
+export const confirmFollowRequest = async (axiosPrivateInstance, followerId) => {
+  try {
+    const response = await axiosPrivateInstance.post('/users/follow/confirm', { followerId })
+    return response.data
+  } catch (err) {
+    console.error('API error:', err)
+    throw err
+  }
+}
