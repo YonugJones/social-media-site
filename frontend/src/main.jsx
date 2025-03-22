353f79a8242bc123d19206f41157e9caca59aa07
YonugJones <peterckerfoot@gmail.com>
2025-03-22 17:03:52 -0500

    added wrapped Router with PostProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/AppRoutes.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
+import { PostProvider } from './contexts/PostContext.jsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <PostProvider>
+        <RouterProvider router={router} />
+      </PostProvider>
     </AuthProvider>
   </StrictMode>,
 )
