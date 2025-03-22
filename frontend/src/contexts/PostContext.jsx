4de883b59ac120f02f10f049caebc68211347015
YonugJones <peterckerfoot@gmail.com>
2025-03-22 17:01:31 -0500

    created usePost custom hook

--- frontend/src/contexts/PostContext.jsx
+++ frontend/src/contexts/PostContext.jsx
@@ -5,16 +5,8 @@ const PostContext = createContext()
 export const PostProvider = ({ children }) => {
   const [posts, setPosts] = useState([])
 
-  const addPost = (newPost) => setPosts((prev) => [newPost, ...prev])
-  const editPost = (updatedPost) => setPosts((prev) =>
-    prev.map((post) => (post.id === updatedPost.id ? updatedPost : post))
-  )
-  const deletePost = (postId) => setPosts((prev) =>
-    prev.filter((post) => post.id !== postId)
-  ) 
-
   return (
-    <PostContext.Provider value={{ posts, setPosts, addPost, editPost, deletePost }}>
+    <PostContext.Provider value={{ posts, setPosts }}>
       {children}
     </PostContext.Provider>
   )
