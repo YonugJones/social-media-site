20511a2ac0c18298994ce1506186cda104915547
YonugJones <peterckerfoot@gmail.com>
2025-03-24 15:15:12 -0500

    removed setPost and setPosts

--- frontend/src/hooks/usePostActions.js
+++ frontend/src/hooks/usePostActions.js
@@ -1,17 +1,32 @@
-// custom hook for handling post actions
-import usePost from './usePost'
+// custom hook for performing API calls and returning the updated post(s) to the calling component.
 import useAxiosPrivate from './useAxiosPrivate'
 import { handleApiError } from '../api/apiHelper'
 
 const usePostActions = () => {
-  const { setPosts, setPost } = usePost()
   const axiosPrivate = useAxiosPrivate()
 
-  const newPost = async (content) => {
+  const createPost = async (content) => {
     try {
       const response = await axiosPrivate.post('/posts', { content })
-      const newPost = response.data.data
-      setPosts((prevPosts) => [newPost, ...prevPosts])
+      return response.data.data
+    } catch (err) {
+      handleApiError(err)
+    }
+  }
+
+  const editPost = async (postId, content) => {
+    try {
+      const response = await axiosPrivate.put(`/posts/${postId}`, { content })
+      return response.data.data
+    } catch (err) {
+      handleApiError(err)
+    }
+  }
+
+  const deletePost = async (postId) => {
+    try {
+      await axiosPrivate.delete(`/posts/${postId}`)
+      return postId
     } catch (err) {
       handleApiError(err)
     }
@@ -20,37 +35,72 @@ const usePostActions = () => {
   const toggleLike = async (postId) => {
     try {
       const response = await axiosPrivate.post(`/posts/${postId}/like`)
-      const updatedPost = response.data.data
-      setPost((prevPost) => 
-        prevPost.map((post) => post.id === postId ? updatedPost : post)
-      )
+      return response.data.data
     } catch (err) {
       handleApiError(err)
     }
   }
 
-  const handleEdit = async (postId, content) => {
-    try {
-      const response = await axiosPrivate.put(`/posts/${postId}`, { content })
-      const updatedPost = response.data.data
-      setPosts((prev) =>
-        prev.map((post) => post.id === postId ? updatedPost : post)
-      )
-    } catch (err) {
-      handleApiError(err)
-    }
-  }
-
-  const handleDelete = async (postId) => {
-    try {
-      await axiosPrivate.delete(`/posts/${postId}`)
-      setPosts((prev) => prev.filter((post) => post.id !== postId))
-    } catch (err) {
-      handleApiError(err)
-    }
-  }
-
-  return { newPost, toggleLike, handleEdit, handleDelete }
+  return { createPost, editPost, deletePost, toggleLike }
 }
 
 export default usePostActions
+
+// OLD CODE
+
+// const usePostActions = () => {
+//   const { setPosts, setPost } = usePost()
+//   const axiosPrivate = useAxiosPrivate()
+
+//   const newPost = async (content) => {
+//     try {
+//       const response = await axiosPrivate.post('/posts', { content })
+//       const newPost = response.data.data
+//       setPosts((prevPosts) => [newPost, ...prevPosts])
+//     } catch (err) {
+//       handleApiError(err)
+//     }
+//   }
+
+//   const toggleLike = async (postId, isSinglePostDisplay = false) => {
+//     try {
+//       const response = await axiosPrivate.post(`/posts/${postId}/like`)
+//       const updatedPost = response.data.data
+
+//       if (isSinglePostDisplay) {
+//         setPost(updatedPost)
+//       } else {
+//         setPosts((prevPost) => 
+//           prevPost.map((post) => post.id === postId ? updatedPost : post)
+//         )
+//       }
+//     } catch (err) {
+//       handleApiError(err)
+//     }
+//   }
+
+//   const handleEdit = async (postId, content) => {
+//     try {
+//       const response = await axiosPrivate.put(`/posts/${postId}`, { content })
+//       const updatedPost = response.data.data
+//       setPosts((prev) =>
+//         prev.map((post) => post.id === postId ? updatedPost : post)
+//       )
+//     } catch (err) {
+//       handleApiError(err)
+//     }
+//   }
+
+//   const handleDelete = async (postId) => {
+//     try {
+//       await axiosPrivate.delete(`/posts/${postId}`)
+//       setPosts((prev) => prev.filter((post) => post.id !== postId))
+//     } catch (err) {
+//       handleApiError(err)
+//     }
+//   }
+
+//   return { newPost, toggleLike, handleEdit, handleDelete }
+// }
+
+// export default usePostActions
