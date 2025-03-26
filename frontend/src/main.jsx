31eacdf5660d252ade8e85403963c64e7ac12c62
YonugJones <peterckerfoot@gmail.com>
2025-03-26 10:15:42 -0500

    wrapped router with UserProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/AppRoutes.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import { PostProvider } from './contexts/PostContext.jsx'
+import { UserProvider } from './contexts/UserContext.jsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
       <PostProvider>
-        <RouterProvider router={router} />
+        <UserProvider>
+          <RouterProvider router={router} />
+        </UserProvider>
       </PostProvider>
     </AuthProvider>
   </StrictMode>,
