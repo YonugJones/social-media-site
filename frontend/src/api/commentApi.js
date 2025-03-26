842c6f9cbdfca03c92dacdad28b858dcf2077088
YonugJones <peterckerfoot@gmail.com>
2025-03-26 07:54:33 -0500

    removed unnecessary code

--- frontend/src/api/commentApi.js
+++ frontend/src/api/commentApi.js
@@ -8,32 +8,32 @@
 //   }
 // }
 
-export const toggleLikeComment = async (axiosPrivateInstance, postId, commentId) => {
-  try {
-    const response = await axiosPrivateInstance.post(`/posts/${postId}/comments/${commentId}/like`)
-    return response.data
-  } catch (err) {
-    console.error('API error:', err)
-    throw err
-  }
-}
+// export const toggleLikeComment = async (axiosPrivateInstance, postId, commentId) => {
+//   try {
+//     const response = await axiosPrivateInstance.post(`/posts/${postId}/comments/${commentId}/like`)
+//     return response.data
+//   } catch (err) {
+//     console.error('API error:', err)
+//     throw err
+//   }
+// }
 
-export const editComment = async (axiosPrivateInstance, postId, commentId, content) => {
-  try {
-    const response = await axiosPrivateInstance.put(`/posts/${postId}/comments/${commentId}`, { content })
-    return response.data
-  } catch (err) {
-    console.error('API error:', err)
-    throw err
-  }
-}
+// export const editComment = async (axiosPrivateInstance, postId, commentId, content) => {
+//   try {
+//     const response = await axiosPrivateInstance.put(`/posts/${postId}/comments/${commentId}`, { content })
+//     return response.data
+//   } catch (err) {
+//     console.error('API error:', err)
+//     throw err
+//   }
+// }
 
-export const deleteComment = async (axiosPrivateInstance, postId, commentId) => {
-  try {
-    const response = await axiosPrivateInstance.delete(`/posts/${postId}/comments/${commentId}`)
-    return response.data
-  } catch (err) {
-    console.error('API error:', err)
-    throw err
-  }
-}
+// export const deleteComment = async (axiosPrivateInstance, postId, commentId) => {
+//   try {
+//     const response = await axiosPrivateInstance.delete(`/posts/${postId}/comments/${commentId}`)
+//     return response.data
+//   } catch (err) {
+//     console.error('API error:', err)
+//     throw err
+//   }
+// }
