73b8998930c3e6c8d31ba67c47bb79bae69d48dc
YonugJones <peterckerfoot@gmail.com>
2025-03-26 19:27:26 -0500

    moved to custom hook

--- frontend/src/api/userApi.js
+++ frontend/src/api/userApi.js
@@ -1,32 +1,32 @@
-export const getUser = async (axiosPrivateInstance, userId) => {
-  try {
-    const response = await axiosPrivateInstance.get(`/users/${userId}`)
-    return response.data
-  } catch (err) {
-    console.error('API error:', err)
-    throw err
-  }
-}
+// export const getUser = async (axiosPrivateInstance, userId) => {
+//   try {
+//     const response = await axiosPrivateInstance.get(`/users/${userId}`)
+//     return response.data
+//   } catch (err) {
+//     console.error('API error:', err)
+//     throw err
+//   }
+// }
 
 
-export const getPostsByUser = async (axiosPrivateInstance, userId) => {
-  try {
-    const response = await axiosPrivateInstance.get(`/users/${userId}/posts`)
-    return response.data
-  } catch (err) {
-    console.error('API error:', err)
-    throw err
-  }
-}
+// export const getPostsByUser = async (axiosPrivateInstance, userId) => {
+//   try {
+//     const response = await axiosPrivateInstance.get(`/users/${userId}/posts`)
+//     return response.data
+//   } catch (err) {
+//     console.error('API error:', err)
+//     throw err
+//   }
+// }
 
-export const editUser = async (axiosPrivateInstance, userId, userData) => {
-  try {
-    const response = await axiosPrivateInstance.put(`/users/${userId}`, userData)
-    return response.data
-  } catch (err) {
-    console.error('API error:', err)
-    throw err
-  }
-}
+// export const editUser = async (axiosPrivateInstance, userId, userData) => {
+//   try {
+//     const response = await axiosPrivateInstance.put(`/users/${userId}`, userData)
+//     return response.data
+//   } catch (err) {
+//     console.error('API error:', err)
+//     throw err
+//   }
+// }
 
-// need to make deleteUser
+// // need to make deleteUser
