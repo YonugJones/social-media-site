3fc0224d9b72d4f25f20db54938508e10205ecbc
YonugJones <peterckerfoot@gmail.com>
2025-03-27 19:42:07 -0500

    created FriendshipContext

--- frontend/src/contexts/FriendshipContext.jsx
+++ frontend/src/contexts/FriendshipContext.jsx
@@ -3,9 +3,14 @@ import { createContext, useState } from 'react'
 const FriendshipContext = createContext()
 
 export const FriendshipProvider = ({ children }) => {
-  const [friendship, setFriendship] = useState([])
-  
-  return 
+  const [followers, setFollowers] = useState([])
+  const [following, setFollowing] = useState([])
+
+  return (
+    <FriendshipContext.Provider value={{ followers, setFollowers, following, setFollowing }}>
+      {children}
+    </FriendshipContext.Provider>
+  )
 }
 
 export default FriendshipContext
