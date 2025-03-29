5e17b96952394a9ecb8a4200c50d65b569ec9ee8
YonugJones <peterckerfoot@gmail.com>
2025-03-29 14:39:43 -0500

    added pendingRequests to state management

--- frontend/src/contexts/FriendshipContext.jsx
+++ frontend/src/contexts/FriendshipContext.jsx
@@ -5,9 +5,12 @@ const FriendshipContext = createContext()
 export const FriendshipProvider = ({ children }) => {
   const [followers, setFollowers] = useState([])
   const [following, setFollowing] = useState([])
+  const [pendingRequests, setPendingRequests] = useState([])
 
   return (
-    <FriendshipContext.Provider value={{ followers, setFollowers, following, setFollowing }}>
+    <FriendshipContext.Provider 
+      value={{ followers, setFollowers, following, setFollowing, pendingRequests, setPendingRequests }}
+    >
       {children}
     </FriendshipContext.Provider>
   )
