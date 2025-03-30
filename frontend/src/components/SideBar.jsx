9c410428def31b7a4c1f268850ddce784f4a84b9
YonugJones <peterckerfoot@gmail.com>
2025-03-30 11:57:01 -0500

    removed unused code and added Users link

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -5,7 +5,6 @@ import styles from '../styles/SideBar.module.css'
 const SideBar = () => {
   const { auth } = useAuth()
 
-
   return (
     <div className={styles['side-bar']}>
       <ul className={styles['side-bar-links']}>
@@ -16,7 +15,10 @@ const SideBar = () => {
           <Link to={`/profile/${auth.id}`} className={styles['link']}>PROFILE</Link>
         </li>
         <li className={styles['friendship-requests-link']}>
-         <Link to={`/profile/${auth.id}/follow-requests`} className={styles['link']}>FRIENDSHIP REQUESTS</Link>
+          <Link to={`/profile/${auth.id}/follow-requests`} className={styles['link']}>FRIENDSHIP REQUESTS</Link>
+        </li>
+        <li className={styles['users-link']}>
+          <Link to={`/friendship/${auth.id}`} className={styles['link']}>USERS</Link>
         </li>
       </ul>
     </div>
@@ -24,70 +26,3 @@ const SideBar = () => {
 }
 
 export default SideBar
-
-// import { useState, useEffect } from 'react'
-// import { getNonFollowing, sendFollowRequest } from '../api/friendshipApi'
-// import NonFollowing from './NonFollowing'
-// import useAxiosPrivate from '../hooks/useAxiosPrivate'
-// import useAuth from '../hooks/useAuth'
-// import { Link } from 'react-router-dom'
-// import styles from '../styles/SideBar.module.css'
-
-// const SideBar = () => {
-//   const { auth } = useAuth()
-//   const axiosPrivate = useAxiosPrivate()
-//   const [pendingRequests, setPendingRequests] = useState([])
-//   const [showNonFollowing, setShowNonFollowing] = useState(false)
-//   const [nonFollowing, setNonFollowing] = useState([])
-
-//   useEffect(() => {
-//     if (showNonFollowing) {
-//       const fetchNonFollowing = async () => {
-//         try {
-//           const response = await getNonFollowing(axiosPrivate, auth.id)
-//           setNonFollowing(response.data)
-//         } catch (err) {
-//           console.error('Failed to fetch non following users', err)
-//         }
-//       }
-//       fetchNonFollowing()
-//     }
-//   }, [showNonFollowing, axiosPrivate, auth.id])
-
-//   const handleAction = async (action, userId) => {
-//     if (action === 'follow') {
-//       await sendFollowRequest(axiosPrivate, userId)
-//       setPendingRequests((prev) => [...prev, userId])
-//       setNonFollowing((prev) => prev.filter(user => user.id !== userId))
-//     }
-//   }
-
-//   return (
-//     <div className={styles['side-bar']}>
-//       <ul className={styles['side-bar-links']}>
-//         <li className={styles['home-link']}>
-//           <Link to='/' className={styles['link']}>HOME</Link>
-//         </li>
-//         <li className={styles['profile-link']}>
-//           <Link to={`/profile/${auth.id}`} className={styles['link']}>PROFILE</Link>
-//         </li>
-//         <li className={styles['find-users-list-item']}>
-//           <button className={styles['find-users-button']} onClick={() => setShowNonFollowing(!showNonFollowing)}>
-//             FIND USERS
-//           </button>
-//         </li>
-//       </ul>
-//       {showNonFollowing && (
-//         <NonFollowing 
-//           nonFollowing={nonFollowing}
-//           pendingRequests={pendingRequests}
-//           onClose={() => setShowNonFollowing(false)}
-//           onAction={handleAction}
-//           profileOwnerId={auth.id}
-//         />
-//       )}
-//     </div>
-//   )
-// }
-
-// export default SideBar
