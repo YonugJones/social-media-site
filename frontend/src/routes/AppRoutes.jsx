07b57dad049796eb8fe96af98f44fa89170f0ac4
YonugJones <peterckerfoot@gmail.com>
2025-03-29 20:44:33 -0500

    added Followers and Following routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -8,6 +8,8 @@ import Feed from '../pages/Feed'
 import Profile from '../pages/Profile'
 import PostDetails from '../pages/PostDetails'
 import FriendshipRequests from '../pages/FriendshipRequests'
+import Followers from '../pages/Followers'
+import Following from '../pages/Following'
 
 const router = createBrowserRouter([
   {
@@ -26,7 +28,9 @@ const router = createBrowserRouter([
           { path: '/', element: <Feed /> },
           { path: '/profile/:userId', element: <Profile /> },
           { path: '/posts/:postId', element: <PostDetails /> },
-          { path: '/profile/:userId/follow-requests', element: <FriendshipRequests /> }
+          { path: '/profile/:userId/follow-requests', element: <FriendshipRequests /> },
+          { path: '/friendship/:userId/followers', element: <Followers /> },
+          { path: '/friendship/:userId/following', element: <Following /> }
         ]
       }
     ]
